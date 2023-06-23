fe446ac5eace1701eee8de14483ac4f6ad5855de
a.krainova <a.krainova@rt-solar.ru>
2023-06-23 16:55:53 +0300

    Update db_mssql.py

--- fixture/db_mssql.py
+++ fixture/db_mssql.py
@@ -1,7 +1,8 @@
-import pymssql
+# import pymssql
 
 
 class MSSQL:
+    pass
 
     def __init__(self, host, name, user, pwd, port, db_type='mssql'):
         self.host = host
@@ -10,9 +11,9 @@ class MSSQL:
         self.pwd = pwd
         self.port = port
         self.db_type = db_type
-        self.connection = pymssql.connect(host=host, port=int(port), user=user, password=pwd, database=name)
+        # self.connection = pymssql.connect(host=host, port=int(port), user=user, password=pwd, database=name)
 
-    def destroy(self):
-        self.connection.close()
+    # def destroy(self):
+    #     self.connection.close()
 
     # todo: сюда по аналогии с class Postgresql необходимо добавить методы для создания пользователя в БД
