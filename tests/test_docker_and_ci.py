29ab45efead22a0e4c6a92012a67d901648429cc
a.krainova <a.krainova@rt-solar.ru>
2023-06-27 22:10:02 +0300

    Update test_docker_and_ci.py

--- tests/test_docker_and_ci.py
+++ tests/test_docker_and_ci.py
@@ -1,7 +1,7 @@
 import allure
 
 
-@allure.title('Автотест для проверки работы docker и github actions')
+@allure.title('Автотест для проверки работы docker и jenkins')
 def test_docker_and_ci():
     with allure.step('Test step'):
         print('It works!')
